86303e3cbc2b032a187c8c97637e581ca16e3176
İlyas Bozdemir <Bozdemir.ib70@gmail.com>
2023-01-09 20:05:00 +0300

    updated

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,39 +1,37 @@
 import { Box, Stack, Text, Flex, HStack } from "@chakra-ui/react";
 import React from "react";
 import Link from "next/link";
-import Logo from './Logo'
+import Logo from "./Logo";
 export default function Header() {
-
   return (
     <>
-    <HStack
-            as={Flex}
-            w={"full"}
-            h={10}
-            zIndex={2}
-            top={0}
-            left={0}
-         
-            position="absolute"
-            justifyContent={"space-between"}
-            textAling={"center"}
-          >
-            <Logo/>
-            <Stack direction={"row"} spacing={6}>
-              <Link href={"/"} legacyBehavior>
-                <a>Anasayfa</a>
-              </Link>
-              <Link href={"/hakkimizda"} legacyBehavior>
-                <a>Hakkımızda</a>
-              </Link>
-              <Link href={"/galeri"} legacyBehavior>
-                <a>Galeri</a>
-              </Link>
-              <Link href={"/iletisim"} legacyBehavior>
-                <a>İletişim</a>
-              </Link>
-            </Stack>
-          </HStack>
+      <HStack
+        as={Flex}
+        w={"full"}
+        h={10}
+        zIndex={2}
+        top={0}
+        left={0}
+        position="absolute"
+        justifyContent={"space-between"}
+        textAling={"center"}
+      >
+        <Logo />
+        <Stack direction={"row"} spacing={6}>
+          <Link href={"/"} legacyBehavior>
+            <a>Anasayfa</a>
+          </Link>
+          <Link href={"/hakkimizda"} legacyBehavior>
+            <a>Hakkımızda</a>
+          </Link>
+          <Link href={"/galeri"} legacyBehavior>
+            <a>Galeri</a>
+          </Link>
+          <Link href={"/iletisim"} legacyBehavior>
+            <a>İletişim</a>
+          </Link>
+        </Stack>
+      </HStack>
     </>
   );
 }
--- components/Hotel.jsx
+++ components/Hotel.jsx
@@ -1,8 +1,9 @@
-import { Box, Stack, Text, Flex, HStack } from "@chakra-ui/react";
+import { Box, Stack, Text, Flex, HStack, Icon,Divider } from "@chakra-ui/react";
 import React from "react";
 import Link from "next/link";
 import TimeBasedImageTimeBasedImage from "./TimeBasedImage";
 import { useState } from "react";
+import { BsFillTelephoneFill } from "react-icons/bs";
 
 function Hotel() {
   const date = new Date();
@@ -26,46 +27,98 @@ function Hotel() {
             //nightImage={"/images/otel-dis-gunduz.jpeg"}
             nightImage={"/images/otel-dis-gece.jpeg"}
           />
-
-          <HStack
-            as={Flex}
-            w={"full"}
-            h={10}
+          <Stack
+            direction={"column"}
             zIndex={2}
             top={0}
             left={0}
-            backgroundColor={
-              bg === "dark" ? "rgba(255, 255, 255, 0.5)" : "rgba(0, 0, 0, 0.5)"
-            }
             position="absolute"
-            justifyContent={"space-between"}
-            textAling={"center"}
+            h={20}
+            as={Flex}
+            w={"full"}
+            spacing={0}
           >
-            <Text
-              color={bg !== "dark" ? "white" : "black"}
-              fontSize="3xl"
-              fontFamily={"Georgia"}
-              cursor={'pointer'}
+            <HStack
+              as={Flex}
+              h={20}
+              backgroundColor={bg === "dark" ? "white" : "black"}
+              justifyContent={"space-between"}
+              textAling={"center"}
             >
-              <Link href={"/"} legacyBehavior>
-                <a>Güven Otel</a>
-              </Link>
-            </Text>
-            <Stack direction={"row"} spacing={6}>
-              <Link href={"/"} legacyBehavior>
-                <a>Anasayfa</a>
-              </Link>
-              <Link href={"/hakkimizda"} legacyBehavior>
-                <a>Hakkımızda</a>
-              </Link>
-              <Link href={"/galeri"} legacyBehavior>
-                <a>Galeri</a>
-              </Link>
-              <Link href={"/iletisim"} legacyBehavior>
-                <a>İletişim</a>
-              </Link>
-            </Stack>
-          </HStack>
+              <HStack
+                color={bg !== "dark" ? "white" : "black"}
+                fontSize={13}
+                fontFamily={"Georgia"}
+                spacing={3}
+                p={3}
+              >
+                <Icon as={BsFillTelephoneFill} fontSize={18}/>
+                <> Rezervasyon: </>
+                <Link href={"tel:+903387368005"} legacyBehavior>
+                  <a>0338 736 80 05</a>
+                </Link>
+                <Icon as={BsFillTelephoneFill} fontSize={18}/>
+                
+                <Link href={"tel:+905435403160"} legacyBehavior>
+                  <a>0543 540 31 60</a>
+                </Link>
+
+              </HStack>
+              <HStack
+                color={bg !== "dark" ? "white" : "black"}
+                fontSize={13}
+                fontFamily={"Georgia"}
+                spacing={3}
+                p={3}
+              >
+               
+              </HStack>
+            </HStack>
+            <Divider/>
+            <HStack
+              as={Flex}
+              w={"full"}
+              h={10}
+              backgroundColor={bg === "dark" ? "white" : "black"}
+              justifyContent={"space-between"}
+              textAling={"center"}
+            >
+              <Text
+                color={bg !== "dark" ? "white" : "black"}
+                fontSize="3xl"
+                fontFamily={"Georgia"}
+                cursor={"pointer"}
+              >
+                <Link href={"/"} legacyBehavior>
+                  <Text cursor={"pointer"} as={"a"}>
+                    Güven Otel
+                  </Text>
+                </Link>
+              </Text>
+              <Stack direction={"row"} spacing={6}>
+                <Link href={"/"} legacyBehavior>
+                  <Text cursor={"pointer"} as={"a"}>
+                    Anasayfa
+                  </Text>
+                </Link>
+                <Link href={"/hakkimizda"} legacyBehavior>
+                  <Text cursor={"pointer"} as={"a"}>
+                    Hakkımızda
+                  </Text>
+                </Link>
+                <Link href={"/galeri"} legacyBehavior>
+                  <Text cursor={"pointer"} as={"a"}>
+                    Galeri
+                  </Text>
+                </Link>
+                <Link href={"/iletisim"} legacyBehavior>
+                  <Text cursor={"pointer"} as={"a"}>
+                    İletişim
+                  </Text>
+                </Link>
+              </Stack>
+            </HStack>
+          </Stack>
         </Box>
       </Box>
     </>
