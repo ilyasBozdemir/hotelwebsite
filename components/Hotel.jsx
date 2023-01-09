5632b8cc30d5bd56d41efdceda882ac1f1ed6d95
İlyas Bozdemir <Bozdemir.ib70@gmail.com>
2023-01-09 11:54:07 +0300

    updated

---
+++ components/Hotel.jsx
@@ -0,0 +1,16 @@
+import { Box, Heading, Image, Text } from "@chakra-ui/react";
+import React from "react";
+
+function Hotel() {
+  return (
+    <>
+      <Box pos={"absolute"}>
+        <Box>
+          <Image src="/images/otel-dis-gunduz.jpeg" alt="Guven-otel" />
+        </Box>
+      </Box>
+    </>
+  );
+}
+
+export default Hotel;
