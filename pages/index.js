49dc0e78e5a9420497355d174ed9424ddc822cc5
İlyas Bozdemir <Bozdemir.ib70@gmail.com>
2023-01-09 21:11:03 +0300

    updated

--- pages/index.js
+++ pages/index.js
@@ -50,6 +50,7 @@ export default function Home() {
         <meta name="publisher" content="ilyas Bozdemir" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="”canonical”" href="/" />
       </Head>
       <Stack as={"main"}>
         <Suspense fallback={<div>Yükleniyor...</div>}>
