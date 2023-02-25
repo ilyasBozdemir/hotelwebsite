4884a9b9e99a833443d39e465209ac0cf556dd5e
İlyas Bozdemir <52322835+ilyasBozdemir@users.noreply.github.com>
2023-02-25 10:58:48 +0300

    updated

---
+++ components/Map.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import dynamic from 'next/dynamic';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
+const MapContainer = dynamic(() => import('react-leaflet').then((mod) => mod.MapContainer), {
+    ssr: false,
+});
+const TileLayer = dynamic(() => import('react-leaflet').then((mod) => mod.TileLayer), {
+    ssr: false,
+});
+const Marker = dynamic(() => import('react-leaflet').then((mod) => mod.Marker), {
+    ssr: false,
+});
+const Popup = dynamic(() => import('react-leaflet').then((mod) => mod.Popup), {
+    ssr: false,
+});
+function Map() {
+    const [positions, setPositions] = React.useState([
+        [36.680261, 32.807540],
+    ]);
+    const [map, setMap] = React.useState(null);
+
+    React.useEffect(() => {
+        if (!map) {
+            setMap(
+                <div style={{ height: '250px', width: '100%' }}>
+                    <MapContainer
+                        center={positions[0]}
+                        zoom={14}
+                        style={{ height: '100%', width: '100%' }}
+                        whenCreated={setMap}
+                    >
+                        <TileLayer
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        {positions.map((position, index) => (
+                            <Marker key={index} position={position}>
+                                <Popup >Güven Otel</Popup>
+                            </Marker>
+                        ))}
+                    </MapContainer>
+                </div>
+            );
+        }
+    }, []);
+
+    return <>{map}</>;
+}
+
+const DynamicMap = dynamic(() => Promise.all([import('leaflet-defaulticon-compatibility')])
+    .then(() => Map), {
+    ssr: false,
+});
+
+export default DynamicMap;
